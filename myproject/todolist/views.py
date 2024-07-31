2f2d2f50a4e74c564d09eb994cf87aec2d632ec3
Artem Bastrygin <352537@mail.ru>
2024-08-01 01:02:50 +0700

    Основная ветка

---
+++ myproject/todolist/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def base(request):
+    return render(request, 'todolist/base.html')
+
+
+def about(request):
+    return render(request, 'todolist/about.html')
